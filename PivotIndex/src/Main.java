9fcdb5bd5d96b8342ae386ce7f6e40f5a7a2962f
olejatorqq <oleg.orlowsky@gmail.com>
2023-03-12 18:37:11 +0300

    PivotIndex

--- PivotIndex/src/Main.java
+++ PivotIndex/src/Main.java
@@ -1,7 +1,5 @@
-import java.util.*;
 
 public class Main {
-
     public static void main(String[] args) {
         int[] array = {1,7,3,6,5,6};
         System.out.print(Solution.pivotIndex(array));
